0c20eb97d0f154ea1b35012755a9846c24e463dc
gamerover98 <gamerover98@hotmail.it>
2023-12-31 14:14:03 +0100

    Reconsideration on char substitution if the string is not LITERAL and fixed spaces issue before \n
    (cherry picked from commit 7365eb8acc9770218bf907ee9c63717c385468ea)

--- src/test/java/ch/jalu/configme/properties/StringPropertyTest.java
+++ src/test/java/ch/jalu/configme/properties/StringPropertyTest.java
@@ -155,7 +155,7 @@ class StringPropertyTest {
         // set a multiple-line strings
         configurationData.setValue(l1Property, "First row\nSecond row");
         configurationData.setValue(l2Property, "First row\r\b\f\nSecond row ");
-        configurationData.setValue(l3Property, "First text\tSecond text");
+        configurationData.setValue(l3Property, "First text Second text");
         configurationData.setValue(l4Property, "[{\r\nenabled : true \r\n}]");
 
         // when
@@ -170,7 +170,7 @@ class StringPropertyTest {
         assertThat(l2, equalTo("First row\nSecond row"));
 
         String l3 = l3Property.determineValue(propertyReader).getValue();
-        assertThat(l3, equalTo("First text    Second text"));
+        assertThat(l3, equalTo("First text Second text"));
 
         String l4 = l4Property.determineValue(propertyReader).getValue();
         assertThat(l4, equalTo("[{\nenabled : true\n}]"));
@@ -187,7 +187,7 @@ class StringPropertyTest {
                     "l2: |-\n" +
                     "    First row\n" +
                     "    Second row\n" +
-                    "l3: First text    Second text\n" +
+                    "l3: First text Second text\n" +
                     "l4: |-\n" +
                     "    [{\n" +
                     "    enabled : true\n" +
